425e245024e1d831548c6defffd7d43c6ef95b69
willard-yuan <yongyuanstu@gmail.com>
2025-10-22 08:29:19 +0800

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -60,6 +60,20 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning={true}>
+      <head>
+        {/* Google tag (gtag.js) */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-W1834E98M7"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-W1834E98M7');
+            `,
+          }}
+        />
+      </head>
       <body className={`${inter.className} relative`}>
         <ThemeProvider
           attribute="class"
